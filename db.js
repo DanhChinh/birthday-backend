140801842798b1687e408b4b13bdb921009c7312
chinh <danhchinh2024@gmail.com>
2025-04-14 01:07:41 +0700

    mysql -> PostGreSQL

--- db.js
+++ db.js
@@ -1,21 +1,21 @@
-// db.js
 require('dotenv').config();
-const mysql = require('mysql2');
-console.log(`host: ${process.env.DB_HOST} \nuser: ${process.env.DB_USER} \npassword: ${process.env.DB_PASS} \ndatabase: ${process.env.DB_NAME}`)
+const { Client } = require('pg'); // Thêm thư viện PostgreSQL
 
-const db = mysql.createConnection({
+console.log(`host: ${process.env.DB_HOST} \nuser: ${process.env.DB_USER} \npassword: ${process.env.DB_PASS} \ndatabase: ${process.env.DB_NAME}`);
+
+const db = new Client({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    port:process.env.DB_PORT
+    port: process.env.DB_PORT, // Cổng kết nối PostgreSQL, mặc định là 5432
 });
 
 db.connect(err => {
     if (err) {
-        console.error('❌ Kết nối MySQL thất bại:', err);
+        console.error('❌ Kết nối PostgreSQL thất bại:', err);
     } else {
-        console.log('✅ Kết nối MySQL thành công');
+        console.log('✅ Kết nối PostgreSQL thành công');
     }
 });
 
