b48b54941e0c1174ba2fba74d2f5a6a6a94140db
chinh <danhchinh2024@gmail.com>
2025-04-14 00:47:40 +0700

    add db_port->db

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    port:process.env.PORT
+    port:process.env.DB_PORT
 });
 
 console.log(`host: ${process.env.DB_HOST} \nuser: ${process.env.DB_USER} \npassword: ${process.env.DB_PASS} \ndatabase: ${process.env.DB_NAME}`)
